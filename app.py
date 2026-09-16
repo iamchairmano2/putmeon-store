812b6d22f6e5d5eb4a9daa059752e4d5b385da18
IAMCHAIRMANO <owusuchairman818@gmail.com>
2026-09-16 06:06:08 +0000

    Initial commit

--- app.py
+++ app.py
@@ -293,7 +293,8 @@ def delete_product(product_id):
     return redirect(url_for("admin_dashboard"))
 
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=False)
